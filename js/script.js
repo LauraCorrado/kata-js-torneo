b1bc2f6cc6568cf1103063642861224c1100d204
Corfaya <lauracorrado0696@gmail.com>
2024-11-26 19:02:45 +0100

    [UPD] Milestone 1 + fighters and weapons update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 // * MILESTONE 1: Scelta dell'arma
-
-for (let f=0; f<fighters.length; f++) {
-    let randomWeaponIndex = Math.floor(Math.random() * weapons.length)
-    const fighterPower = fighters[f].power
-    const waeponPower = weapons[randomWeaponIndex].power
-    const power = fighterPower + waeponPower
-    console.log('Potere ' + fighters[f].name + ' iniziale: ' + fighterPower + '. Potere arma '+ weapons[randomWeaponIndex].name + ': ' + waeponPower + '. POTERE FINALE: ' + power)
-    weapons.splice(randomWeaponIndex, 1)
-  }
+for (let f = 0; f < fighters.length; f++) {
+  let randomWeaponIndex = Math.floor(Math.random() * weapons.length);
+  const fighterPower = fighters[f].power;
+  const waeponPower = weapons[randomWeaponIndex].power;
+  const power = fighterPower + waeponPower;
+  console.log(
+    "Potere " +
+      fighters[f].name +
+      " iniziale: " +
+      fighterPower +
+      ". Potere arma " +
+      weapons[randomWeaponIndex].name +
+      ": " +
+      waeponPower +
+      ". POTERE FINALE: " +
+      power
+  );
+  weapons.splice(randomWeaponIndex, 1);
+}
